24d7fff494810b74b19883c97461d1e319078068
chimamario <mariochima646@gmail.com>
2026-09-20 08:17:18 -0600

    stress test 2 complete

--- main_numbering_algo.py
+++ main_numbering_algo.py
@@ -45,7 +45,7 @@ def show_matrix(matrix):
     plt.title("13 x 13 Grid")
     plt.show()
 
-def show_matrix_v2(boolean_matrix, final_matrix):
+def show_matrix_v2(boolean_matrix, final_matrix,main_number_dict, current_set):
 
     boolean_matrix = np.array(boolean_matrix, dtype=float)
 
@@ -90,6 +90,23 @@ def show_matrix_v2(boolean_matrix, final_matrix):
     )
 
     plt.title("13 x 13 Grid")
+
+    if current_set:
+        plt.title(f"Failed on current set: {current_set}")
+        remaining_numbers = main_number_dict.values()
+        extra_info = f"Remaining numbers left: {remaining_numbers}"
+        plt.figtext(
+        0.78, 0.5, 
+        extra_info, 
+        fontsize=10, 
+        va='center',          # Vertically centers the text box
+        ha='left',            # Aligns text inside to the left
+        multialignment='left',
+        bbox=dict(boxstyle='round,pad=0.5', facecolor='whitesmoke', edgecolor='gray', alpha=0.5)
+        )
+    else:
+        plt.title("Complete grid!!")
+
     plt.show()
 
 
@@ -97,14 +114,14 @@ def show_matrix_v2(boolean_matrix, final_matrix):
 
 if __name__ == "__main__":
     #Puzzle 11
-    # boolean_matrix, main_number_dict = extract_matrix_and_nums("test_photo_v3.jpg", add_nums=['533', '133', '3273'], remove_nums=['0420'], confirmed = False) 
+    # boolean_matrix, main_number_dict = extract_matrix_and_nums("test_photo_v3.jpg", add_nums=['533', '133', '3273'], remove_nums=['0420'], confirmed = True) 
 
     #Puzzle 12
-    boolean_matrix, main_number_dict = extract_matrix_and_nums("Testing Folder/Puzzle 12 v2.jpg", remove_nums=['141', '229'], confirmed = False) 
+    boolean_matrix, main_number_dict = extract_matrix_and_nums("Testing Folder/Puzzle 12 v2.jpg", remove_nums=['141', '229'], confirmed = True) 
    
 
     #initial matrix without inputs
-    show_matrix(boolean_matrix)
+    # show_matrix(boolean_matrix)
     # correct_matrix = input("is matrix correct? [y or n]: ")
     # correct_matrix = correct_matrix.capitalize()
 
@@ -112,8 +129,8 @@ if __name__ == "__main__":
     if correct_matrix == 'Y':
         # main_number_dict = create_number_dict(numbers)
         attempt1 = MatrixCoordinator(None,boolean_matrix.copy(), main_number_dict)
-        final_matrix = attempt1.main_coordinator()
-        show_matrix_v2(boolean_matrix, final_matrix)
+        final_matrix, main_number_dict, current_set = attempt1.main_coordinator()
+        show_matrix_v2(boolean_matrix, final_matrix, main_number_dict, current_set)
     else:
         print("Boolean_matrix is incorrect. Readjust photo.")
 
